43ecee7f8c783c922ddba629c4e55bfed7c0684c
Brooks Cutter <brooksc@brooksc.com>
2013-02-22 18:24:54 -0500

    added findpos

--- brooksc_findpos.py
+++ brooksc_findpos.py
@@ -9,10 +9,14 @@ import time
 import server
 
 if __name__ == "__main__":
-#    while True:
-    #Find out your players position
     mc = minecraft.Minecraft.create(server.address)
-    playerPos = mc.player.getPos()
-    mc.postToChat("Find your position - its x=%s z=%s y=%s" % (int(playerPos.x), int(playerPos.z), int(playerPos.y)))
-#    mc.setBlock(0,0,0,block.DIAMOND_BLOCK)
-#    time.sleep(1)
+    mc.postToChat("Flattening surface")
+    mc.setBlocks(-128,0,-128,128,64,128,0)
+    mc.setBlocks(-128,0,-128,128,-64,128,block.SANDSTONE.id)
+    mc.postToChat("Putting a diamong block at 0,0,0")
+    mc.setBlock(0,0,0,block.DIAMOND_BLOCK.id)
+    while True:
+        #Find out your players position
+        playerPos = mc.player.getPos()
+        mc.postToChat("Find your position - its x=%s z=%s y=%s" % (int(playerPos.x), int(playerPos.z), int(playerPos.y)))
+        time.sleep(1)
